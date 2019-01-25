3dbc23f99f3716a7c49eaf3bec73d7467816aca4
Bea Huszár <husz.beata@gmail.com>
2019-01-25 12:10:23 +0100

    add: User table

--- src/main/java/com/springsecu/mysql/model/User.java
+++ src/main/java/com/springsecu/mysql/model/User.java
@@ -1,4 +1,34 @@
 package com.springsecu.mysql.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+  @Entity
+  @Table(name = "user")
+  public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "active")
+    private int active;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public Users() {
+    }
 }
