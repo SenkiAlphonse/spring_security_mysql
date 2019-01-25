c6d270ffd2e1070370c3f683a5407646a82dbe7c
Bea Huszár <husz.beata@gmail.com>
2019-01-25 14:47:50 +0100

    add: custom user detail service logic

--- src/main/java/com/springsecu/mysql/repository/UserRepository.java
+++ src/main/java/com/springsecu/mysql/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.springsecu.mysql.repository;
 import com.springsecu.mysql.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+  Optional<User> findByName(); //mark it as optional, because it can happen that there is no such name - Java8 function to handle null cases
 }
--- src/main/java/com/springsecu/mysql/service/CustomUserDetailsService.java
+++ src/main/java/com/springsecu/mysql/service/CustomUserDetailsService.java
@@ -1,14 +1,30 @@
 package com.springsecu.mysql.service;
 
+import com.springsecu.mysql.model.CustomUserDetails;
+import com.springsecu.mysql.model.User;
+import com.springsecu.mysql.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    return null;
+    Optional<User> optionalUser = userRepository.findByName(); //this way no nullcheck it needed
+
+    optionalUser
+        .orElseThrow(() -> new UsernameNotFoundException("Username not found")); //if no user is found, throw an exception
+
+    return optionalUser
+        .map(CustomUserDetails::new).get();
   }
 }
