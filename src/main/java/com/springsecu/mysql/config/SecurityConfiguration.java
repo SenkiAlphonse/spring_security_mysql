932e6507f0a2dd8a6f09a6322d78ea997f758ce9
Bea Huszár <husz.beata@gmail.com>
2019-01-25 14:54:44 +0100

    add: security config logic

--- src/main/java/com/springsecu/mysql/config/SecurityConfiguration.java
+++ src/main/java/com/springsecu/mysql/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.springsecu.mysql.config;
 
 import com.springsecu.mysql.repository.UserRepository;
+import com.springsecu.mysql.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity //enables spring security
 @EnableJpaRepositories(basePackageClasses = UserRepository.class) //injects all the classes that we need from the JPArepo
@@ -17,10 +19,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   // spring maintaining the username and password
 
   @Autowired
-  private UserDetailsService userDetailsService; //it is an interface, implemented here
+  private CustomUserDetailsService userDetailsService; //it is an interface, implemented here
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(userDetailsService); //connects to the database and gets the data
+    auth.userDetailsService(userDetailsService)
+    .passwordEncoder(getPasswordEncoder()); //connects to the database and gets the data
+  }
+
+  private PasswordEncoder getPasswordEncoder() {
+    return new PasswordEncoder() {
+      @Override
+      public String encode(CharSequence charSequence) {
+        return charSequence.toString();
+      }
+
+      @Override
+      public boolean matches(CharSequence charSequence, String s) {
+        return true;
+      }
+    };
   }
 }
