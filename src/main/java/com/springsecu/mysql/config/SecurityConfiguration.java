149e1cc44fd421594392b0950c3efb199f7c8a78
Bea Huszár <husz.beata@gmail.com>
2019-01-25 16:43:36 +0100

    add: password encoder & endpoint authorisation

--- src/main/java/com/springsecu/mysql/config/SecurityConfiguration.java
+++ src/main/java/com/springsecu/mysql/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,7 +28,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     .passwordEncoder(getPasswordEncoder()); //connects to the database and gets the data
   }
 
-  private PasswordEncoder getPasswordEncoder() {
+  //security for http rest endpoints
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable(); //disable cross-site reference
+    http.authorizeRequests()
+        .antMatchers("**/secured/**").authenticated() //authorize all the requests for these endpoints and use the configure function above
+        .anyRequest().permitAll() //any other requests(endpoints) can be called w/o authentication
+        .and().formLogin().permitAll(); //move to default spring security login page if no authentication is found
+                //for custom login page the url can be given here .formlogin.loginPage("/myLoginPage....")
+  }
+
+  private PasswordEncoder getPasswordEncoder() { //23:00 - explanation
     return new PasswordEncoder() {
       @Override
       public String encode(CharSequence charSequence) {
